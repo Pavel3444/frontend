2767bbc9a1d1d4a462038bfd6549431ae58cf011
Pavel3444 <burikov.pavel3444@yandex.ru>
2023-08-23 12:51:28 +0300

    added pwa

--- react-router/src/index.js
+++ react-router/src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
 
+if ('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/sw.js')
+    .then((reg)=>{
+        console.log('%cService worker has been registered.', 'color: blue; font-size: larger; font-weight: bold');
+        console.log(reg)
+    })
+    .catch((err)=>{
+        console.log('%cService worker has not registered.', 'color: red; font-size: larger; font-weight: bold');
+        console.log(err);
+    });
+}
+
 const App = lazy(()=>import('./App'))
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
